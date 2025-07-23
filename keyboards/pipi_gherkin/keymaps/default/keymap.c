fa6da66aa0f856214fd5fb4df9f515898ffe0340
Taro Hayashi <87086895+Taro-Hayashi@users.noreply.github.com>
2025-07-23 12:20:48 +0900

    initial

---
+++ keyboards/pipi_gherkin/keymaps/default/keymap.c
@@ -0,0 +1,12 @@
+// Copyright 2021 Hayashi (@w_vwbw)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT( /* NONE */
+        KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
+        KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_0,
+        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_1, KC_2, KC_3
+  )
+};
