f7afe5c662777b40a15aa57a19275a0280827eb4
Taro Hayashi <87086895+Taro-Hayashi@users.noreply.github.com>
2025-07-23 12:53:04 +0900

    keymap

---
+++ keyboards/pipi_gherkin/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+// Copyright 2021 Hayashi (@w_vwbw)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x67, 0xD6, 0xC9, 0x98, 0x0E, 0x0E, 0xBB, 0x06}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
